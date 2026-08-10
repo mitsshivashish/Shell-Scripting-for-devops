dbf65ac66d5eb8ab3477026fb962a69a7599635a
mitsshivashish <Shivashishtiwari18@gmail.com>
2026-08-11 00:57:29 +0530

    Learned about local and global scope

---
+++ Day 04 Functions/functions_scope.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# TODO: Implement safe_inc() that echoes (n + 1) without mutating globals.
+# - Accept one integer argument.
+# - Return non-zero for invalid input.
+
+counter=100  # global that must not be modified
+
+safe_inc() {
+  # your code here
+  local counter=$1
+  [[ $counter =~ ^-?[0-9]+$ ]] && counter=$(( $counter + 1)) || return 1
+  echo "$counter"
+}
+
+echo "global before: $counter"
+safe_inc 41   # -> 42
+echo "global after:  $counter"  # should remain 100
+
