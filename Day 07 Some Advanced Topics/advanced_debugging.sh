8c6cb8ff700b4fb28c8717a786033a70b1cf56ca
mitsshivashish <Shivashishtiwari18@gmail.com>
2026-08-15 19:40:28 +0530

    Learned some debugging

---
+++ Day 07 Some Advanced Topics/advanced_debugging.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Demonstrate pipefail by failing early in a pipeline
+
+# TODO: Enable pipefail and create a pipeline that returns non-zero
+set -o pipefail; false | true; echo $? # -> 1
